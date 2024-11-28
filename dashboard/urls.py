2f8c08a47758d8955aabb550373c71c10207c37f
Ermias Asmare <it.ermias.asmare@gmail.com>
2024-11-28 10:02:20 +0300

    fixed some typos

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
-    path('change-password/', PasswordChangeView.as_view(template_name='userAuth/change_password.html', success_url='/dashboard/'), name='change_password'),
+    path('change-password/', PasswordChangeView.as_view(template_name='dashboard/change_password.html', success_url='/dashboard/'), name='change_password'),
     path('forgot-password/', views.forgot_password_view, name='forgot_password'),
     path('reset-password/<uidb64>/<token>/', views.reset_password_view, name='reset_password'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
