ee7c6b7ab56b8bc4e64c72bbcfdca5c0008f3706
Ermias Asmare <it.ermias.asmare@gmail.com>
2024-11-28 09:21:50 +0300

    created the login view for the login functionality

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    if request.method == 'POST':
+        try:
+            username = request.POST['username']
+            password = request.POST['password']
+        except KeyError:
+            messages.error(request, 'Please provide both username and password.')
+            return render(request, 'userAuth/login.html')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid username or password.')
+
+    return render(request, 'userAuth/login.html')
